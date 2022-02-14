a730ce97051f6acec40cc9ce3a426ca3023fd55f
Sebastian Riedel <git@basxto.de>
2022-02-14 19:28:35 +0100

    Add support for header checksum

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # TitCHack
-Title Checksum Hack for CGB in DMG mode.
+Title and Header Checksum Hack for CGB in DMG mode. It’s pronouced “tight check”.
 
-Fixes given header byte ([0x134-0x143](https://gbdev.io/pandocs/The_Cartridge_Header.html#0134-0143---title)) to match given checksum.
+Fixes given header byte ([0x134-0x143](https://gbdev.io/pandocs/The_Cartridge_Header.html#0134-0143---title)) to match given title checksum.
 
+Or fixes given header byte (0x134-0x14C) to match [header checksum](https://gbdev.io/pandocs/The_Cartridge_Header.html#014d---header-checksum).
+
+# Title Checksum
 Palettes can be seletced with the [correct checksum](https://tcrf.net/Notes:Game_Boy_Color_Bootstrap_ROM) if [old licensee](https://gbdev.io/pandocs/The_Cartridge_Header.html#014b---old-licensee-code) is set to 1 (Nintendo).
 
 ## Example
@@ -10,7 +13,7 @@ Set palette of ROM `myrom.cgb` to "Link's Awakening" (`0x70`) via title byte `0x
 
 ```sh
 rgbfix -l 1 myrom.cgb
-./titchack.py myrom.cgb '$142' '0x70'
+./titchack.py myrom.cgb '$142' -c'0x70'
 rgbfix -f h myrom.cgb
 ```
 
@@ -18,8 +21,19 @@ The makebin equivalent to [`rgbfix -l 1`](https://rgbds.gbdev.io/docs/v0.5.1/rgb
 ## Notice
 This only changes the specified byte from the title (0x134-0x143), which makes the checksums in the header incorrect. Palettes use the title checksum, which has nothing to do with the header checksum or global checksum.
 
-You still need to run [`rgbfix -f h`](https://rgbds.gbdev.io/docs/v0.5.1/rgbfix.1#f) (only header, will boot) or `rgbfix -f hg` (header and global) to fix those.
+You still need to run [`rgbfix -f h`](https://rgbds.gbdev.io/docs/v0.5.1/rgbfix.1#f) (only header, will boot) or `rgbfix -f hg` (header and global) to fix those. Or perform a header checksum hack with another run.
 
 Some original roms had checksum collision, those are distinguished by the [4th character of the title](https://github.com/ISSOtm/gb-bootroms/blob/6232573bc6592df17cdbce878c418e79d8355b68/src/cgb.asm#L1273), which has to be set beforehand.
 
 Be warned that [0x143](https://gbdev.io/pandocs/The_Cartridge_Header.html#0143---cgb-flag) set to the wrong number can screw up your ROM. If it enables CGB mode, predefined palettes are not used and you have to define colors yourself. If it enables PGB mode, you basically end up with a white on white palette.
+
+# Header Checksum
+
+## Example
+
+```sh
+./titchack.py -theader myrom.cgb '$13F'
+```
+
+## Notice
+The header checksum can’t be fixed with a title byte if you also want to hack the title checksum.
--- titchack.py
+++ titchack.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-Title Checksum Hack
-Copyright 2021 Sebastian "basxto" Riedel
+Previously: Title Checksum Hack
+Copyright 2021-2022 Sebastian "basxto" Riedel
 [License: MIT]
 
 Fixes the title to match the given checksum
@@ -24,6 +24,11 @@ def str2int(x):
         y = int(x, base=10)
     return y
 
+def wrapAddress(addr, maxa):
+    while addr > maxa:
+        addr = addr - maxa
+    return addr
+
 def parse_argv(argv):
     p = argparse.ArgumentParser()
     p.add_argument("file")
@@ -32,23 +37,51 @@ def parse_argv(argv):
     return p.parse_args(argv[1:])
 
 def main(argv=None):
-    args = parse_argv(argv or sys.argv)
-    address = str2int(args.address)
-    checksum = str2int(args.checksum)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--type", "-t", default="title", help="'title' (default) or 'header' checksum")
+    parser.add_argument("--checksum", "-c", help="Target checksum (only for title checksum)")
+    parser.add_argument("--offset", default="0x100", help="Offset where header begins (default: 0x100)")
+    #parser.add_argument("--64b", default="no", help="64b mirrored ROM")
+    parser.add_argument("file", help="ROM file that should be fixed")
+    parser.add_argument("address", help="Address of the byte that should be fixed")
+    global args
+    args = parser.parse_args()
+    if args.type != "header":
+        if not args.checksum:
+            sys.stderr.write("error: 'title' type needs a checksum\n")
+            sys.exit(2)
 
-    if (address < 0x134 or address > 0x143):
-        print('Address has to be between 0x134 and 0x143')
+    offset = str2int(args.offset)
+    maxa = 0x80 # maximum address at which to wrap
+    base = 0x34 # base address for the checksum
+    if args.type != "header":
+        amount = 16
+    else:
+        amount = 25
+    # translate address alreaty
+    address = wrapAddress(str2int(args.address) - offset, maxa)
+
+    # minimum/maxim wrapped address
+    minwa = wrapAddress(base, maxa)
+    maxwa = wrapAddress(base + amount, maxa)
+    if (address < minwa or address > maxwa):
+        print('Address has to be between 0x{:04x} and 0x{:04x}'.format(minwa, maxwa))
         exit()
 
     with open(args.file, "rb+") as infp:
-        infp.seek(0x134)
-        data = infp.read(25)
+        infp.seek(offset)
+        data = infp.read(maxa)
+        if args.type != "header":
+            checksum = str2int(args.checksum)
+        else:
+            checksum = data[wrapAddress(0x4D, maxa)]
+            print("Actual header checksum is 0x{:02x}".format(checksum))
 
         # substract the wanted checksum from the real one
         checksum = -checksum
-        for i in range(0, 16):
-            if (0x134+i) != address:
-                checksum += data[i]
+        for i in range(0, amount):
+            if wrapAddress(base + i, maxa) != address:
+                checksum += data[wrapAddress(base + i, maxa)]
         # fix up if still negative
         if checksum < 0:
             checksum = 0xFF - checksum
@@ -56,10 +89,14 @@ def main(argv=None):
         # calculate the 8b inverse in two's complement
         checksum = 0xFF - ((checksum-1) & 0xFF)
 
-        infp.seek(address)
+        # header and title checksum are calculated slightly differently
+        if args.type == "header":
+            checksum = checksum + 1
+
+        infp.seek(offset + address)
         infp.write(checksum.to_bytes(1, 'little'))
 
-    print("Byte 0x{:02x} set to 0x{:02x}".format(address, checksum))
+    print("Byte 0x{:02x} set to 0x{:02x}".format(offset + address, checksum))
 
 
 if __name__=='__main__':
