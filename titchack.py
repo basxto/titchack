8652006cee3f658be1d8a33b5d00e83ccdc10b18
Sebastian Riedel <git@basxto.de>
2022-02-15 03:11:41 +0100

    Print values for debugging

--- titchack.py
+++ titchack.py
@@ -36,13 +36,14 @@ def main(argv=None):
     parser.add_argument("--type", "-t", default="title", help="'title' (default) or 'header' checksum")
     parser.add_argument("--checksum", "-c", help="Target checksum (only for title checksum)")
     parser.add_argument("--offset", default="0x100", help="Offset where header begins (default: 0x100)")
-    parser.add_argument("--mirror64", default="no", help="64b mirrored ROM (default: no)")
-    parser.add_argument("--ambiguous", "-a", default="no", help="Set 4th byte for ambiguous titles (default: no)")
+    parser.add_argument("--mirror64", "-6", default="no", help="64b mirrored ROM (default: no)")
+    parser.add_argument("--ambiguous", "-a", default="no", help="Set 4th char for ambiguous titles (default: no)")
+    parser.add_argument("--print", "-p", default="no", help="Just print the checksums (default: no)")
     parser.add_argument("file", help="ROM file that should be fixed")
     parser.add_argument("address", help="Address of the byte that should be fixed")
     global args
     args = parser.parse_args()
-    if args.type != "header":
+    if args.type != "header" and args.print == "no":
         if not args.checksum:
             sys.stderr.write("error: 'title' type needs a checksum\n")
             sys.exit(2)
@@ -72,39 +73,56 @@ def main(argv=None):
             exit()
 
     with open(args.file, "rb+") as infp:
-        # fix the fourth byte of title
-        if len(args.ambiguous) == 1:
-            infp.seek(offset + 0x38)
+        # fix the fourth char of title
+        if len(args.ambiguous) == 1 and args.print == "no":
+            infp.seek(offset + base + 4)
             infp.write(bytes(args.ambiguous, "ascii"))
 
         infp.seek(offset)
         data = infp.read(maxa)
-        if args.type != "header":
+        if args.type != "header" and args.print == "no":
             checksum = str2int(args.checksum)
         else:
             checksum = data[0x4D % maxa]
-            print("Actual header checksum is 0x{:02x}".format(checksum))
-
-        # substract the wanted checksum from the real one
-        checksum = -checksum
-        for i in range(0, amount):
-            if ((base + i) % maxa) != address:
+            print("[0x{:02x}] Header checksum byte is 0x{:02x}".format(offset + (0x4D % maxa), checksum))
+        if args.print != "no":
+            if chr(data[(base + 4) % maxa]).isprintable():
+                print("[0x{0:02x}] Ambiguity char is '{1:c}' (0x{1:02x})".format(offset + ((base + 4) % maxa), data[(base + 4) % maxa]))
+            else:
+                print("[0x{0:02x}] Ambiguity char is 0x{1:02x}".format(offset + ((base + 4) % maxa), data[(base + 4) % maxa]))
+            if chr(data[address]).isprintable():
+                print("[0x{0:02x}] Byte is '{1:c}' (0x{1:02x})".format(offset + address, data[address]))
+            else:
+                print("[0x{0:02x}] Byte is 0x{1:02x}".format(offset + address, data[address]))
+            # we want to calculate the checksum, not the fix for it
+            checksum = 0
+            for i in range(0, 16):
                 checksum += data[(base + i) % maxa]
-        # fix up if still negative
-        if checksum < 0:
-            checksum = 0xFF - checksum
+            print("Real title checksum is 0x{:02x}".format(checksum & 0xFF))
+            for i in range(16, 25):
+                checksum += data[(base + i) % maxa]
+            print("Real header checksum is 0x{:02x}".format((checksum-1) & 0xFF))
+        else:
+            # substract the wanted checksum from the real one
+            checksum = -checksum
+            for i in range(0, amount):
+                if ((base + i) % maxa) != address:
+                    checksum += data[(base + i) % maxa]
+            # fix up if still negative
+            if checksum < 0:
+                checksum = 0xFF - checksum
 
-        # calculate the 8b inverse in two's complement
-        checksum = 0xFF - ((checksum-1) & 0xFF)
+            # calculate the 8b inverse in two's complement
+            checksum = 0xFF - ((checksum-1) & 0xFF)
 
-        # header and title checksum are calculated slightly differently
-        if args.type == "header":
-            checksum = checksum + 1
+            # header and title checksum are calculated slightly differently
+            if args.type == "header":
+                checksum = checksum + 1
 
-        infp.seek(offset + address)
-        infp.write(checksum.to_bytes(1, 'little'))
+            infp.seek(offset + address)
+            infp.write(checksum.to_bytes(1, 'little'))
 
-    print("Byte 0x{:02x} set to 0x{:02x}".format(offset + address, checksum))
+            print("Byte 0x{:02x} set to 0x{:02x}".format(offset + address, checksum))
 
 
 if __name__=='__main__':
